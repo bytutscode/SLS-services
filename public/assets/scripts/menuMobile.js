8458e7e9e80d9da4365c19ece8c43d34a94cd9e1
Samuel Lima <samuellimadossantosoficial@gmail.com>
2023-08-06 17:35:03 -0300

    menu mobile bug fixed

--- public/assets/scripts/menuMobile.js
+++ public/assets/scripts/menuMobile.js
@@ -6,4 +6,5 @@ const menuBars = document.querySelectorAll('#menuMobile .bar');
 menuBTN.addEventListener('click', () => {
     menuBTN.classList.toggle('active');
     menu.classList.toggle('active');
+    menu.style.transition = 'all ease-in-out .5s';
 })
