8e0b26027acdc49bf67316d406a2d9f15bc15deb
Samuel <mielszrx@gmail.com>
2024-04-13 10:50:01 -0300

    pg added

--- src/database/SQL.ts
+++ src/database/SQL.ts
@@ -6,8 +6,8 @@ const sequelize = new Sequelize(
     process.env.MYSQL_DB as string,
     process.env.MYSQL_USERNAME as string,
     process.env.MYSQL_PASSWORD as string, {
-    dialect: 'mysql',
-    dialectModule: require('mysql2'),
+    dialect: 'postgres',
+    dialectModule: require('pg'),
     host: process.env.MYSQL_HOST,
     dialectOptions: {
         ssl: {
